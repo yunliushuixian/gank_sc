06bb0f13da400ed299ef16037fec82b98dfe0173
howard <howard.chen@zoom.us>
2019-11-07 15:30:17 +0800

    code merge

---
+++ gank_sc_config_client/src/main/java/com/howard/gank_sc_config_client/GankScConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.howard.gank_sc_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GankScConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GankScConfigClientApplication.class, args);
+    }
+
+}
