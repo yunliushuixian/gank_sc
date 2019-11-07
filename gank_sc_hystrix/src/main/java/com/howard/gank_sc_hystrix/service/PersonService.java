db35022d6f3bb1f16589a1a98ee8bc95d8137a46
howard <howard.chen@zoom.us>
2019-11-07 18:15:32 +0800

    circuitBreaker and rollingStats

--- gank_sc_hystrix/src/main/java/com/howard/gank_sc_hystrix/service/PersonService.java
+++ gank_sc_hystrix/src/main/java/com/howard/gank_sc_hystrix/service/PersonService.java
@@ -26,10 +26,20 @@ public class PersonService {
      * <p>
      * 通过向@HystrixCommand注解中传递额外的参数，可以自定义Hystrix的等待时机;execution.isolation.thread.timeoutInMilliseconds设置超时时间长度
      *
+     *
+     * circuitBreaker.requestVolumeThreshold:控制在Hystrix将考虑调用断路器跳闸之前，在一个10秒的窗口内必须出现的连续调用数量
+     * circuitBreaker.errorThresholdPercentage:调用必须失败的百分比（由于超时，将抛出一个异常；或者返回HTTP 500）
+     * circuitBreaker.sleepWindowInMilliseconds:一旦断路跳闸,Hystrix允许另外的调用通过来看服务是否恢复健康之前Hystrix将休眠的时间
+     *
+     * metrics.rollingStats.timeInMilliseconds,用于控制窗口的大小，默认是10秒
+     * metrics.rollingStats.numBuckets:控制在窗口中定义并被收集的统计次数。
+     * Hystrix将使用15秒窗口并将统计数据收集到五个长度为三秒的桶中。进入的统计窗口越小，窗口中保存的桶数越大，在高容量服务上就可以提高CPU和内存利用率。
+     *
+     *
      * @param id
      * @return
      */
-    @HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000")}, fallbackMethod = "getDefaultPersonInfo", threadPoolKey = "personPool", threadPoolProperties = {@HystrixProperty(name = "coreSize", value = "3"), @HystrixProperty(name = "maxQueueSize", value = "10")})
+    @HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000"), @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "75"), @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "7000"), @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "15000"), @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "5")}, fallbackMethod = "getDefaultPersonInfo", threadPoolKey = "personPool", threadPoolProperties = {@HystrixProperty(name = "coreSize", value = "3"), @HystrixProperty(name = "maxQueueSize", value = "10")})
     public Person getPersonByName(String id) {
         System.out.println(Thread.currentThread().getId());
         //randomRunLong();
